aad39e22adb4609a381eeed7f46bd1a3aad2bd5f
Dan <d@dup.bz>
2022-07-06 18:01:31 -0400

    Reduce settings padding for mobile

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -21,7 +21,7 @@ const Modal = ({openState, children}: ModalProps) => {
 	return open ?
 		<div id="modal-wrapper">
 				<div id="modal-overlay" className="bg-black opacity-[.92] absolute top-0 right-0 bottom-0 left-0" onClick={() => setOpen(false)}/>
-				<div id="modal" className="bg-[#27272A] p-10 absolute top-[50vh] left-[50vw] translate-x-[-50%] border-2 border-color-[#F4F4F4] min-w-[320px]">
+				<div id="modal" className="bg-[#27272A] p-4 md:p-10 absolute top-[50vh] left-[50vw] translate-x-[-50%] border-2 border-color-[#F4F4F4] min-w-[320px]">
 					{children}
 				</div>
 		</div> : <></>
