c53efc632f44b443aa49b4a282a0e1fc5c153c44
Dan <d@dup.bz>
2022-07-09 18:11:37 -0400

    Add pagination to footer

---
+++ components/Pagination/index.tsx
@@ -0,0 +1,32 @@
+import { translations as t } from '../../i18n'
+import { PaginationProps } from '../../types'
+import { useRouter } from 'next/router'
+
+const Pagination = ({ meta, pageState, locale }: PaginationProps) => {
+	const { setPage } = pageState
+	const router = useRouter()
+
+	return <>
+		{(meta.hasPrevious || meta.hasNext) &&
+			<div className="previous">
+				<button onClick={() => setPage(prevState => prevState - 1)} disabled={!meta.hasPrevious} title={t[locale].previous}>
+					←
+				</button>
+			</div>
+		}
+		<div className="index">
+			<button onClick={() => router.push('/')} title={t[locale].index}>
+				↑
+			</button>
+		</div>
+		{(meta.hasPrevious || meta.hasNext) &&
+			<div className="next">
+				<button onClick={() => setPage(prevState => prevState + 1)} disabled={!meta.hasNext} title={t[locale].next}>
+					→
+				</button>
+			</div>
+		}
+	</>
+}
+
+export default Pagination
