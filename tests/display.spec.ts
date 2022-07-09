4700244f97917b1f697ad518e33944916dd2e13e
Dan <d@dup.bz>
2022-07-09 17:00:09 -0400

    Add test for settings button

--- tests/display.spec.ts
+++ tests/display.spec.ts
@@ -1,9 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test'
 
-const host = 'http://localhost:1234';
+const host = 'http://localhost:1234'
 
 const glob = require('glob')
 const path = require('path')
+
+const navigateToFirstAccount = async (page: Page) => {
+	await page.goto(host + '/')
+	const accountName = await page.locator('li a >> nth=0').innerText()
+
+	await Promise.all([
+		page.waitForResponse(resp => {
+			return resp.url().includes('/api/files?account=' + accountName) && resp.status() === 200
+		}),
+		await page.locator('li a >> nth=0').click()
+	])
+
+	await expect(page).toHaveURL(host + '/' + accountName)
+}
 
 test.describe('Tests page display', () => {
 	test('displays list of accounts on homepage', async ({ page }) => {
@@ -19,16 +33,14 @@ test.describe('Tests page display', () => {
 	})
 
 	test('clicks an account and loads account page', async ({ page }) => {
-		await page.goto(host + '/')
-		const accountName = await page.locator('li a >> nth=0').innerText()
+		await navigateToFirstAccount(page)
+	})
 
-		await Promise.all([
-			page.waitForResponse(resp => {
-				return resp.url().includes('/api/files?account=' + accountName) && resp.status() === 200
-			}),
-			await page.locator('li a >> nth=0').click()
-		])
-
-		await expect(page).toHaveURL(host + '/' + accountName);
+	test('opens settings', async ({ page }) => {
+		// Settings are per-account, have to go to an account page
+		await navigateToFirstAccount(page)
+		await expect(page.locator('#settings-form')).not.toBeVisible()
+		await page.locator('#button-container-settings button').click()
+		await expect(page.locator('#settings-form')).toBeVisible()
 	})
 })
